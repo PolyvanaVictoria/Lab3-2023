86053bd6b49b1feed98d578b9966635d878dbe66
PolyvanaVictoria <78734134+PolyvanaVictoria@users.noreply.github.com>
2023-05-29 16:56:29 +0200

    new one

--- src/main/kotlin/ua/kpi/its/lab/rest/repository/Repositories.kt
+++ src/main/kotlin/ua/kpi/its/lab/rest/repository/Repositories.kt
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import ua.kpi.its.lab.rest.entity.Hospital
 import ua.kpi.its.lab.rest.entity.Medicine
 
-interface ЛікарняRepository : JpaRepository<Hospital, Long>
+interface HospitalRepository : JpaRepository<Hospital, Long>
 
 interface MedicineRepository : JpaRepository<Medicine, Long>
 
