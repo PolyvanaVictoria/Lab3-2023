60c9c3ddcc66c969fa53270945e596901440e073
PolyvanaVictoria <78734134+PolyvanaVictoria@users.noreply.github.com>
2023-05-30 16:52:29 +0200

    first commit

--- src/main/kotlin/ua/kpi/its/lab/rest/config/WebSecurityConfigurerAdapter.kt
+++ src/main/kotlin/ua/kpi/its/lab/rest/config/WebSecurityConfigurerAdapter.kt
@@ -1,2 +1,9 @@
-package ua.kpi.its.lab.rest.config 
+package ua.kpi.its.lab.rest.config
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+
+
+open class WebSecurityConfigurerAdapter {
+
+    open fun configure(http: HttpSecurity) {}
+}
--- src/main/kotlin/ua/kpi/its/lab/rest/init/SecurityWebInitializer.kt
+++ src/main/kotlin/ua/kpi/its/lab/rest/init/SecurityWebInitializer.kt
@@ -1,13 +1,14 @@
 package ua.kpi.its.lab.rest.init
 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
 import ua.kpi.its.lab.rest.config.RootConfig
 import ua.kpi.its.lab.rest.config.WebConfig
 
-class WebInitializer : AbstractAnnotationConfigDispatcherServletInitializer() {
-    override fun getServletMappings(): Array<String> = arrayOf("/")
+class SecurityWebInitializer : AbstractSecurityWebApplicationInitializer() {
+    fun getServletMappings(): Array<String> = arrayOf("/")
 
-    override fun getRootConfigClasses(): Array<Class<*>> = arrayOf(RootConfig::class.java)
+    fun getRootConfigClasses(): Array<Class<*>> = arrayOf(RootConfig::class.java)
 
-    override fun getServletConfigClasses(): Array<Class<*>> = arrayOf(WebConfig::class.java)
+    fun getServletConfigClasses(): Array<Class<*>> = arrayOf(WebConfig::class.java)
 }
